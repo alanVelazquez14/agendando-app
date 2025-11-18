c5793e4efcc3bfec35f4ace9e0f0ba13ff6306d4
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-11-17 23:10:02 -0300

    feat: modificaciones en general

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -14,24 +14,23 @@ export default function Home() {
         </div>
         <div className="space-y-10 text-center max-w-4xl">
           <div>
-            <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl text-foreground mb-6 text-pretty leading-tight">
-              Gestioná tus turnos de forma{" "}
+            <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl text-foreground mb-10 mt-20">
+              Gestioná tus turnos sin esfuerzo, {""}
               <span className="text-[#651B1B] dark:text-[#9A2C2C]">
-                simple y profesional
+                las 24 horas.
               </span>
             </h1>
 
             <p className="text-lg md:text-xl max-w-2xl text-center mx-auto mb-10 text-gray-600">
-              Automatizá tus reservas, organizá tu agenda y mejorá la
-              experiencia de tus clientes en segundos.
+              Un sistema inteligente que automatiza reservas, recordatorios y horarios sin que tengas que hacerlo vos.
             </p>
           </div>
 
-          <div className="flex flex-col sm:flex-row gap-4 justify-center mt-20">
+          <div className="flex flex-col sm:flex-row gap-20 justify-center mt-20">
             <button className="bg-[#651B1B] dark:bg-[#9A2C2C] text-white px-5 py-2 rounded-xl font-bold shadow-md hover:bg-[#651B1B]/90 dark:hover:bg-[#9A2C2C]/90 transition-all duration-300 transform hover:-translate-y-0.5 focus:ring-4 focus:ring-[#651B1B]/50 dark:focus:ring-[#9A2C2C]/50">
-              Comenzar gratis <ArrowRight className="inline-block ml-2" />
+              Comenzar gratis <ArrowRight className="inline-block ml-5" />
             </button>
-            <a href="#como-funciona" className="px-8 py-3 rounded-xl border-2 border-[var(--success)] text-[var(--success)] hover:bg-[var(--success)]/30 font-bold bg-transparent shadow-md transition-all duration-300 transform hover:-translate-y-0.5 focus:ring-4 focus:ring-[var(--success)]/50">
+            <a href="#como-funciona" className="px-8 py-3 rounded-xl border-2 border-green-700 text-green-700 font-bold hover:bg-green-700 hover:text-white shadow-md transition-all duration-300 transform hover:-translate-y-0.5 focus:ring-4">
               Ver cómo funciona
             </a>
           </div>
