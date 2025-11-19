b649266db3abd9a2680aa2ef1475d06eca5f3e04
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-11-19 18:41:53 -0300

    feat: /auth/register funcionando

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,22 +1,32 @@
 import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { User } from "../models/User";
+import pool from "../database/db.js";
 
 export const register = async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
 
-    const exists = await User.findOne({ email });
-    if (exists) return res.status(400).json({ message: "Email ya registrado" });
+    if (!name || !email || !password)
+      return res.status(400).json({ message: "Faltan datos" });
+
+    const exists = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (exists.rows.length > 0)
+      return res.status(400).json({ message: "Email ya registrado" });
 
     const hashed = await bcrypt.hash(password, 10);
 
-    const newUser = new User({ email, password: hashed });
-    await newUser.save();
+    await pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+      [name, email, hashed]
+    );
 
     res.status(201).json({ message: "Usuario registrado" });
-  } catch (err) {
+  } catch (err: any) {
+    console.error(err);
     res.status(500).json({ message: "Error en el servidor" });
   }
 };
@@ -25,22 +35,42 @@ export const login = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
-    if (!user)
+    if (!email || !password)
+      return res.status(400).json({ message: "Faltan datos" });
+
+    // 1. Buscar usuario
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (result.rows.length === 0)
       return res.status(400).json({ message: "Credenciales inválidas" });
 
+    const user = result.rows[0];
+
+    // 2. Comparar contraseña
     const match = await bcrypt.compare(password, user.password);
+
     if (!match)
       return res.status(400).json({ message: "Credenciales inválidas" });
 
+    // 3. Generar token
     const token = jwt.sign(
-      { userId: user._id },
+      { userId: user.id },
       process.env.JWT_SECRET as string,
       { expiresIn: "1d" }
     );
 
-    res.json({ token });
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
   } catch (err) {
-    res.status(500).json({ message: "Error en el servidor" });
+    console.error("LOGIN ERROR:", err);
+    return res.status(500).json({ message: "Error en el servidor" });
   }
 };
