1100a5d937a3eb5ba955a652abb94ca8a9e26361
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-11-21 10:58:20 -0300

    feat: modificaciones para deploy

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -4,11 +4,10 @@ import { Pool } from 'pg';
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
