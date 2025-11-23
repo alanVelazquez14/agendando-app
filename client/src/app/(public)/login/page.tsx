6450afd6a20c721cdf3a809d89465baf5a29674d
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-11-23 12:50:47 -0300

    feat: modificando para conexion con el back

--- client/src/app/(public)/login/page.tsx
+++ client/src/app/(public)/login/page.tsx
@@ -41,7 +41,7 @@ export default function LoginPage() {
     try {
       setLoading(true);
 
-      const res = await fetch("http://localhost:3001/auth/login", {
+      const res = await fetch(`${process.env.API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
--- client/src/app/(public)/register/page.tsx
+++ client/src/app/(public)/register/page.tsx
@@ -81,7 +81,7 @@ export default function RegisterPage() {
     console.log("ENVIANDO:", formData);
 
     try {
-      const res = await fetch("http://localhost:3001/auth/register", {
+      const res = await fetch(`${process.env.API_URL}/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
