164baa2b1162f0ea7b280f3f374038f563d4eddd
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-11-23 18:46:36 -0300

    feat: modificando para conexion con el back

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -4,12 +4,8 @@ import { Pool } from "pg";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
