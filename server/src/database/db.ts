fc69a1591300195cb6f7fe305abf2352fe7e6b75
alanVelazquez14 <velazquez.alan14@gmail.com>
2025-11-24 13:01:52 -0300

    feat:solucionado lo del deploy en back y front

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -4,8 +4,12 @@ import { Pool } from "pg";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
